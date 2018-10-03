c8086294dd1462067a1554210d61186a427f441e
Satori <comfycallum@gmail.com>
2018-10-03 17:39:18 +0100

    Add test model and viewmodel

---
+++ AzureClientUI/Models/Process.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AzureClientUI.Models
+{
+    public class Process : INotifyPropertyChanged
+    {
+        private int _pid;
+        private string _name;
+        private string _path;
+       
+        public int PID
+        {
+            get => _pid;
+            set
+            {
+                if (_pid == value) return;
+
+                _pid = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name == value) return;
+
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path == value) return;
+
+                _path = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AzureClientUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AzureClientUI.Models;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace AzureClientUI.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public List<Process> Processes { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            Processes = new List<Process>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                Processes.Add(new Process
+                {
+                    PID = 100 + i,
+                    Name = String.Format("Test{0}.exe", i+1),
+                    Path = String.Format("C:\\Windows\\Test{0}.exe", i+1)
+                });
+            }
+        }
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
