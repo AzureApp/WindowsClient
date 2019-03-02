2f97ed01e8f89478803a7790fc1b7536f8ff9a8c
Satori <comfycallum@gmail.com>
2019-03-02 20:37:43 +0000

    Add HandshakeObject
    Updated MsgPack.Cli

---
+++ AzureClientUI/DataObjects/HandshakeObject.cs
@@ -0,0 +1,31 @@
+﻿using MsgPack.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureClientUI.DataObjects
+{
+    public enum OperatingSystem
+    {
+        Windows,
+        iOS,
+        macOS,
+        Android,
+        Linux
+    }
+
+    public class HandshakeObject : MetaObject
+    {
+        [MessagePackMember(2)]
+        public string ACPVersion { get; set; } = "1.0.0";
+        [MessagePackMember(3)]
+        public OperatingSystem System { get; set; } = OperatingSystem.Windows;
+
+        public HandshakeObject()
+        {
+            Type = ObjectType.Handshake;
+        }
+    }
+}
